81685ba6875066292d611a6ec61035077a41f39e
CodifyArhab <fdzakyarhab@gmail.com>
2024-09-22 21:13:30 +0700

    first commit

---
+++ Latihan14AppWisataJepang/app/src/main/java/com/fadhiil2010/latihan_14_app_wisata_jepang/Adapter/WisataAdapter.kt
@@ -0,0 +1,58 @@
+package com.fadhil2010.latihan_14_app_wisata_jepang.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.fadhiil2010.latihan_14_app_wisata_jepang.R
+
+class AdapterGambar (private val context: Context, private val images: Array<Int>,
+                     private val judulnya: Array<String>, private val lokasinya: Array<String>
+):
+    BaseAdapter()
+{
+    override fun getCount(): Int {
+        return images.size
+        return judulnya.size
+        return lokasinya.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return images[position]
+        return judulnya[position]
+        return lokasinya[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val imageView: ImageView
+        val judul : TextView
+        val loc : TextView
+
+        if (convertView == null){
+            view = LayoutInflater.from(context).inflate(R.layout.item_wisata, parent, false)
+            imageView = view.findViewById(R.id.imgMovie)
+            judul = view.findViewById(R.id.txtMovie)
+        } else{
+            view = convertView
+            imageView = view.findViewById(R.id.imgMovie)
+            judul = view.findViewById(R.id.txtMovie)
+        }
+        imageView.setImageResource(images[position])
+        judul.setText(judulnya[position])
+        return view
+
+
+    }
+
+
+
+
+}
---
+++ Latihan14AppWisataJepang/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Latihan-14-App-Wisata-Jepang"
+include(":app")
+ 
